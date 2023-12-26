f8ea8ecf35edbafd309c0c3611f1c5fe282e57f0
rezone <rezoneprinceme@gmail.com>
2023-12-26 06:21:05 +0600

    swip slider added

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,71 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+
+const Category = () => {
+  return (
+    <div className="py-9">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        // modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="relative">
+          <img src={slide1} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Salads
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide2} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Pizza
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide3} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Soups
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide1} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Salads
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide4} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Desserts
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide3} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Soups
+          </h4>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <img src={slide2} alt="" />
+          <h4 className="text-3xl text-white text-center uppercase absolute bottom-0 inset-x-16">
+            Pizza
+          </h4>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
