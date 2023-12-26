959adc8a0f9c64f25d646191c58c4f3602a23975
rezone <rezoneprinceme@gmail.com>
2023-12-27 02:54:13 +0600

    parallax scrool added

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,9 +4,11 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-10 my-10">
-      <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}></SectionTitle>
-      <div className="md:flex justify-center items-center py-20 px-36">
+    <div className="featured-item bg-fixed text-white pt-10 my-20">
+      <div>
+        <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}></SectionTitle>
+      </div>
+      <div className="md:flex justify-center items-center py-20 px-36 bg-slate-600 bg-opacity-30">
         <div>
           <img src={featuredImg} alt="" />
         </div>
@@ -17,7 +19,7 @@ const Featured = () => {
             deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium
             tempore consequatur consequuntur omnis ullam maxime tenetur.
           </p>
-          <button className="btn btn-outline text-white">Order Now</button>
+          <button className="btn btn-outline border-0 border-b-4 text-white">Order Now</button>
         </div>
       </div>
     </div>
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Testimonials = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"---What Our Clients Say---"}
+        heading={"TESTIMONIALS"}
+      ></SectionTitle>
+      ;
+    </section>
+  );
+};
+
+export default Testimonials;
