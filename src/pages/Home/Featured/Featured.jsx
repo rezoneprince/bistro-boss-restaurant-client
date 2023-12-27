a740f110d65a4c220641489c444f4c6eab7365e6
rezone <rezoneprinceme@gmail.com>
2023-12-27 06:01:03 +0600

    featured bg updated

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,22 +4,24 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="featured-item bg-fixed text-white pt-10 my-20">
-      <div>
-        <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}></SectionTitle>
-      </div>
-      <div className="md:flex justify-center items-center py-20 px-36 bg-slate-600 bg-opacity-30">
-        <div>
-          <img src={featuredImg} alt="" />
+    <div className="featured-item bg-fixed text-white my-20">
+      <div className="bg-slate-600 bg-opacity-30">
+        <div className="pt-10">
+          <SectionTitle subHeading={"---Check it out---"} heading={"FROM OUR MENU"}></SectionTitle>
         </div>
-        <div className="md:ml-10">
-          <p className="uppercase">Jan 25, 2023 WHERE CAN I GET SOME?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere,
-            deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium
-            tempore consequatur consequuntur omnis ullam maxime tenetur.
-          </p>
-          <button className="btn btn-outline border-0 border-b-4 text-white">Order Now</button>
+        <div className="md:flex justify-center items-center py-20 px-36">
+          <div>
+            <img src={featuredImg} alt="" />
+          </div>
+          <div className="md:ml-10">
+            <p className="uppercase">Jan 25, 2023 WHERE CAN I GET SOME?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere,
+              deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad
+              laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.
+            </p>
+            <button className="btn btn-outline border-0 border-b-4 text-white">Order Now</button>
+          </div>
         </div>
       </div>
     </div>
