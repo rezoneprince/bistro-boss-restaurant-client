d70d2cab0e31b2371398aff335e38a7db218cb20
rezone <rezoneprinceme@gmail.com>
2023-12-29 06:59:44 +0600

    order page food card tabs added

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import orderCoverImg from "../../../assets/shop/order.jpg";
 import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../Hooks/useMenu";
+
 const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Cover img={orderCoverImg} title={"Order Food"}></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
     </div>
   );
 };
